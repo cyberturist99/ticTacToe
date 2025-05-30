04ebf4a9d43ba3ca22c3e388ace775559ae45a5f
Andrew Labutin <Labutin.99@bk.ru>
2025-05-30 19:44:32 +0700

    added feature to put X and O

--- src/components/Field/Field.tsx
+++ src/components/Field/Field.tsx
@@ -1,7 +1,26 @@
 import "./field.css";
 
-const Field = () => {
-	return <div className="field"></div>;
+interface Props {
+	board: (string | null)[];
+	index: number;
+	handleClick: (index: number) => void;
+}
+
+const Field: React.FC<Props> = ({ handleClick, index, board }) => {
+	const getTextColor = () => {
+		if (board[index] === "X") {
+			return { color: "blue" };
+		} else if (board[index] === "O") {
+			return { color: "green" };
+		} else {
+			return { color: "inherit" };
+		}
+	};
+	return (
+		<div className="field" onClick={() => handleClick(index)}>
+			<span style={getTextColor()}>{board[index]}</span>
+		</div>
+	);
 };
 
 export default Field;
--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -3,13 +3,33 @@ import "./Table.css";
 import { useState } from "react";
 
 export default function Table() {
-	const [board, setBoard] = useState([null, null, null, null, null, null, null, null, null]);
+	const [board, setBoard] = useState<(string | null)[]>(Array(9).fill(null));
+	const [player, setPlayer] = useState("X");
+
+	function resetGame() {
+		setPlayer("X");
+		setBoard(Array(9).fill(null));
+	}
+
+	function handleClick(index: number) {
+		if (board[index] === null) {
+			const newBoard = [...board];
+			newBoard[index] = player;
+			setBoard(newBoard);
+			setPlayer(player === "X" ? "O" : "X");
+		}
+	}
 
 	return (
-		<div className="wrapper">
-			{board.map((_, index) => {
-				return <Field key={index} />;
-			})}
-		</div>
+		<>
+			<div className="wrapper">
+				{board.map((_, index) => {
+					return <Field key={index} index={index} handleClick={handleClick} board={board} />;
+				})}
+			</div>
+			<button className="resetBtn" onClick={resetGame}>
+				Заново
+			</button>
+		</>
 	);
 }
